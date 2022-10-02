7bf690a82db5fad61b4f57d6fdad36ffc4bcf614
khanmohammadimran <khanmohammadimran.i31@gmail.com>
2022-10-03 00:21:24 +0600

    basic setup complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Navbar from "./Pages/Shared/Navbar/Navbar";
+import Home from "./Pages/Homepage/Home";
+import Donate from "./Pages/Donate/Donate";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/donate" element={<Donate />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Donate/Donate.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Donate = () => {
+  return (
+    <div>
+      <h2>This is donate</h2>
+    </div>
+  );
+};
+
+export default Donate;
